453261efad7d82acd029da5fa83e0d6840d96597
umair raza <umairrazanizami@gmail.com>
2019-03-24 00:26:57 +0500

    first

---
+++ app.js
@@ -0,0 +1,166 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = express();
+
+
+const mongoose = require('mongoose')
+
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+
+server.use(bodyParser.urlencoded())
+server.use(bodyParser.json())
+server.use(express.static('./Build'))
+
+
+server.use(session({
+    secret: "secret-word"
+}));
+server.use(passport.initialize());
+server.use(passport.session());
+
+
+
+
+//database connection
+
+mongoose.connect("mongodb://admin:admin1122@ds161024.mlab.com:61024/umair_school_management",{ useNewUrlParser: true } ,() => {
+    console.log("Mubarakaan data base chal gai")
+});
+
+//database models
+
+const Student = mongoose.model('Student', {
+    name: String,
+    fname: String,
+    class: String,
+    DOB: String,
+    mobile: String,
+    address: String,
+    rollNo: Number,
+    fee: Number
+});
+
+const Teacher = mongoose.model('Teacher', {
+    name : String,
+    fname : String,
+    DOB : String,
+    mobile : String,
+    address :String,
+    email : String,
+    qualification : String,
+    rank: String,
+    pay : Number
+});
+
+
+
+
+
+server.post('/addStudents', (req, res) => {
+
+    const student = new Student({
+        name: req.body.name,
+        fname: req.body.fname,
+        class: req.body.class,
+        DOB: req.body.DOB,
+        mobile: req.body.mobile,
+        address: req.body.address,
+        rollNo: req.body.rollNo,
+        fee: req.body.fee
+    });
+
+    student.save(() => {
+        console.log("student is saved");
+    })
+
+    res.send({success : true , data : student})
+})
+
+
+server.post('/addTeacher', (req, res) => {
+
+    const teacher = new Teacher({
+        name: req.body.name,
+        fname: req.body.fname,
+        email: req.body.email,
+        DOB: req.body.DOB,
+        mobile: req.body.mobile,
+        address: req.body.address,
+        qualification: req.body.qualification,
+        rank: 'teacher',
+        pay: req.body.pay
+
+    });
+
+    teacher.save(() => {
+        console.log("teacher is saved");
+    })
+
+    res.send({success : true , data : teacher})
+})
+
+server.post('/showDetails', (req, res) => {
+    let classname = req.body.class;
+    Student.find({class : classname} , (err , student)=> {
+        
+        if(err) return console.log(err);
+        res.send(student)
+
+    })
+})
+
+
+server.get('/getTeachers', (req, res) => {
+    Teacher.find({rank:'teacher'} , (err , teacher) =>{
+        if(err) return console.log(err);
+        res.send(teacher);
+    })
+})
+
+server.post('/showStudent', (req, res) => {
+
+    Student.find({class : req.body.class , rollNo : req.body.rollNo } , (err , student)=> {
+        
+        if(err) return console.log(err);
+        res.send(student)
+
+    })
+})
+
+server.post('/expelStudent', (req, res) => {
+
+    Student.deleteOne({class : req.body.class , rollNo : req.body.rollNo } , (err)=> {
+        
+        if(err) return console.log(err);
+        res.send("Student Is Expelled");
+
+    })
+})
+
+
+server.post('/expelTeacher', (req, res) => {
+
+    Teacher.deleteOne({name: req.body.name } , (err)=> {
+        
+        if(err) return console.log(err);
+        res.send("Teacher Is Expelled");
+
+    })
+})
+
+
+
+
+
+
+server.use((err, req, res, nex) => {
+    console.log(err)
+
+    res.status(500).send('Something went wrong')
+
+})
+
+server.listen( 8080, () => console.log('server is running succesfully'))
---
+++ build/precache-manifest.d14ca9135d1d89abb0162de1b07b5c50.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c30421714327456aea94097d687df01b",
+    "url": "/static/media/parents.c3042171.png"
+  },
+  {
+    "revision": "b5c9844e972937297f2d",
+    "url": "/static/css/main.0433170e.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3bb0b0c2a51d0d1a3136",
+    "url": "/static/js/2.3bb0b0c2.chunk.js"
+  },
+  {
+    "revision": "42f2a47da03743602bf3dfb0ba2b6a49",
+    "url": "/static/media/logo.42f2a47d.png"
+  },
+  {
+    "revision": "fa7c4b02122e1e7e626c8ea91a5b6509",
+    "url": "/static/media/details.fa7c4b02.jpg"
+  },
+  {
+    "revision": "1ef2eb1317d11ecddb49898d98b02258",
+    "url": "/static/media/female-teacher.1ef2eb13.jpg"
+  },
+  {
+    "revision": "39b9bc9380dcbbe38f25ccf7cfe8dae5",
+    "url": "/static/media/class.39b9bc93.jpg"
+  },
+  {
+    "revision": "63a4ce3ec0f71163dea7a493bb11afd9",
+    "url": "/static/media/timetable.63a4ce3e.jpg"
+  },
+  {
+    "revision": "b5c9844e972937297f2d",
+    "url": "/static/js/main.b5c9844e.chunk.js"
+  },
+  {
+    "revision": "825b78a856845bcf467ddbdc97d09c12",
+    "url": "/static/media/fee.825b78a8.jpg"
+  },
+  {
+    "revision": "d385f99fe55971aca3f44460e0d59a2d",
+    "url": "/static/media/addstudent.d385f99f.jpg"
+  },
+  {
+    "revision": "ce1f576fe026eb189bed690e2a47a49c",
+    "url": "/static/media/expelstudent.ce1f576f.jpg"
+  },
+  {
+    "revision": "e15df24b81f86276750ff1b9603391f4",
+    "url": "/static/media/pay.e15df24b.jpg"
+  },
+  {
+    "revision": "32975094a2f43de9534def340e0de757",
+    "url": "/static/media/teacher.32975094.jpg"
+  },
+  {
+    "revision": "a735481626b93fe48a44dfe12dbbe6f5",
+    "url": "/static/media/expelteacher.a7354816.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fa6ed44dd93670c5a74c3a84589a933a",
+    "url": "/index.html"
+  }
+];
